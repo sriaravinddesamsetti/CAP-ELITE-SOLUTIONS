f4234973fb6747d4992f9c9416fec7277e5674e0
sriaravinddesamsetti <103654103+sriaravinddesamsetti@users.noreply.github.com>
2023-01-27 12:12:02 +0530

    Create symtrictree.py

---
+++ 27-01-2023/symtrictree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isSymtrc(n1 = root, n2 = root):
+            if not (n1 or n2):
+                return True
+            elif not (n1 and n2):
+                return False
+            elif n1.val != n2.val:
+                return False
+            return isSymtrc(n1.left, n2.right) and isSymtrc(n1.right, n2.left)
+        return isSymtrc()
